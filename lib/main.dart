0bc6f4ac231f593c32f8f37040f26015f2c147c5
iamshawnsree <yathartha.subedi@gmail.com>
2022-03-05 17:40:00 +0545

    login page and half of bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:musicapp2/screens/Home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:musicapp2/screens/Home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
