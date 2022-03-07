51dcff683346d1439feaf1c04c2155429d99c2aa
Pratik <chhetriprateek5@gmail.com>
2022-03-07 16:47:48 +0545

    profilepage

---
+++ lib/edit_profile_page.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:musicapp2/appbar_widget.dart';
+import 'package:musicapp2/user.dart';
+import 'package:path/path.dart';
+
+import 'profile_widget.dart';
+import 'textfield_widget.dart';
+import 'user_preferences.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  User user = UserPreferences.myUser;
+
+  @override
+  Widget build(BuildContext context) => ThemeSwitchingArea(
+        child: Builder(
+          builder: (context) => Scaffold(
+            appBar: buildAppBar(context),
+            body: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              physics: BouncingScrollPhysics(),
+              children: [
+                ProfileWidget(
+                  imagePath: user.imagePath,
+                  isEdit: true,
+                  onClicked: () async {},
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Full Name',
+                  text: user.name,
+                  onChanged: (name) {},
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Email',
+                  text: user.email,
+                  onChanged: (email) {},
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'About',
+                  text: user.about,
+                  maxLines: 5,
+                  onChanged: (about) {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+}
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/cupertino.dart';
+import 'package:musicapp2/user.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({Key? key}) : super(key: key);
-
+lass Profile extends StatefulWidget {
   @override
-  State<Profile> createState() => _ProfileState();
+  _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("This is profile page...."),
+    final user = UserPreferences.myUser;
+
+    return ThemeSwitchingArea(
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: buildAppBar(context),
+          body: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                imagePath: user.imagePath,
+                onClicked: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => EditProfilePage()),
+                  );
+                },
+              ),
+              const SizedBox(height: 24),
+              buildName(user),
+              const SizedBox(height: 24),
+              Center(child: buildUpgradeButton()),
+              const SizedBox(height: 24),
+              const SizedBox(height: 48),
+              buildAbout(user),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.email,
+            style: TextStyle(color: Colors.grey),
+          )
+        ],
+      );
+
+  Widget buildUpgradeButton() => ButtonWidget(
+        text: 'Upgrade To PRO',
+        onClicked: () {},
+      );
+
+  Widget buildAbout(User user) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'About',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              user.about,
+              style: TextStyle(fontSize: 16, height: 1.4),
+            ),
+          ],
+        ),
+      );
 }
